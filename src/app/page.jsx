543db1ab8c90d08e69d26267ec77a5908236023c
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2024-03-02 12:29:41 +0100

    Update nav css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className='max-2xl:h-[1000px]'>
       <header className='max-2xl:h-[120px] md:h-[100px]'>
-        <nav className='max-2xl:text-center max-2xl:p-[10px] font-bold md:absolute md:flex md:flex-row md:w-[25%] md:right-[250px] md:mt-8 items-center md:place-content-around'>
+        <nav className='max-2xl:text-center max-2xl:p-[10px] font-bold 2xl:absolute max-lg:w-[70%] m-auto max-2xl:w-[50%]  md:flex md:flex-row 2xl:w-[30%] md:right-[250px] md:mt-8 items-center md:place-content-around'>
           <Link href='contact' className='block max-2xl:mb-[10px] max-2xl:w-[50%] max-2xl:m-auto'>Contact</Link>
           <Link href='about' className='block max-2xl:mb-[10px] max-2xl:w-[50%] max-2xl:m-auto'>About</Link>
           <Link href='projets' className='block max-2xl:mb-[10px] max-2xl:w-[50%] max-2xl:m-auto'>Projets</Link>
