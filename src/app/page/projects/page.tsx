c9d7b20694c475729aec6fb256897167d3fb948d
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2024-03-16 15:33:03 +0100

    compress Images, update projects pages

--- src/app/page/projects/page.tsx
+++ src/app/page/projects/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Header from "@/app/components/header"
 import Profile from '/public/img/_DSC5091.jpg';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 export default function Portfolio() {
 
@@ -31,11 +32,11 @@ export default function Portfolio() {
                 </div>
                 <section className="m-auto w-[95%]">
                     <h1 className="text-[30px] m-auto border-b-4 border-gray-300 text-purple-500 font-bold uppercase">Front-End</h1>
-                    {
-                        projects.map((project: any) => {
-                            return (
-                                <div key={project._id} className='pl-[50px] flex  gap-x-20 flex-wrap mb-5'>
-                                    <div className="mt-5 bg-gray-200 drop-shadow-2xl shd w-[20%] max-xl:w-[25%] max-md:w-[35%] h-[300px] rounded-[25px]">
+                    <div className='pl-[50px] flex  gap-x-20 flex-wrap mb-5'>
+                        {
+                            projects.map((project: any) => {
+                                return (
+                                    <div key={project._id} className="mt-5 bg-gray-200 drop-shadow-2xl shd w-[20%] max-xl:w-[25%] max-md:w-[35%] h-[300px] rounded-[25px]">
                                         <div className='relative h-[100px] border-b-4 border-gray-800'>
                                             <Image
                                                 src={project.image}
@@ -49,16 +50,15 @@ export default function Portfolio() {
                                         </div>
                                         <div className='pl-5 pt-2'>
                                             <p className=' font-semibold text-[20px]'>{project.title}</p>
-                                            <p className='mt-5'>{project.description}</p>
-                                            <p className='mt-5'>{project.link}</p>
-                                            <p className='mt-5'>{project.image}</p>
+                                            <p className='mt-5 h-16'>{project.description}</p>
+                                            <Link target='_blank' href={project.link} className='block mt-2 w-[55%] text-center font-semibold text-[15px] text-white rounded-[20px] p-5 bg-purple-500'>Voir {project.title}</Link>
                                         </div>
                                     </div>
-                                </div>
-                            )
+                                )
 
-                        })
-                    }
+                            })
+                        }
+                    </div>
                 </section>
                 <section className="m-auto w-[95%]">
                     <h2 className="text-[30px] m-auto border-b-4 border-gray-300 text-purple-500 font-bold uppercase">Back-End</h2>
