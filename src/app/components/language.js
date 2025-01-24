aa85e18719e0615046f124e124979ea468a97321
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-24 15:05:57 +0100

    ajout de la bdd sqlite

--- src/app/components/language.js
+++ src/app/components/language.js
@@ -2,146 +2,152 @@ import Symfony from "@/../public/img/langage/symfony.png";
 import php from "@/../public/img/langage/php.png";
 
 const Language = [
-    {
-        "id": 1,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/color/480/html-5--v1.png",
-        "alt": "html5s",
-        "type": "frontend",
-    },
+  {
+    id: 1,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/color/480/html-5--v1.png",
+    alt: "html5s",
+    type: "frontend",
+  },
 
-    {
-        "id": 2,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/color/480/css3.png",
-        "alt": "css3",
-        "type": "css",
-    },
+  {
+    id: 2,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/color/480/css3.png",
+    alt: "css3",
+    type: "css",
+  },
 
-    {
-        "id": 3,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png",
-        "alt": "React",
-        "type": "frontend",
-        "link": "https://fr.legacy.reactjs.org/",
-    },
+  {
+    id: 3,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png",
+    alt: "React",
+    type: "frontend",
+    link: "https://fr.legacy.reactjs.org/",
+  },
 
+  {
+    id: 4,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/color/480/tailwindcss.png",
+    alt: "TailwindCss",
+    type: "css",
+    link: "https://tailwindcss.com/",
+  },
 
-    {
-        "id": 4,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/color/480/tailwindcss.png",
-        "alt": "TailwindCss",
-        "type": "css",
-        "link": "https://tailwindcss.com/"
+  {
+    id: 5,
+    width: 96,
+    height: 96,
+    src: Symfony,
+    alt: "Symfony",
+    type: "backend",
+    link: "https://symfony.com/",
+  },
 
-    },
+  {
+    id: 6,
+    width: 96,
+    height: 96,
+    src: php,
+    alt: "php",
+    type: "backend",
+    link: "https://www.php.net/",
+  },
 
-    {
-        "id": 5,
-        "width" : 96,
-        "height": 96,
-        "src": Symfony,
-        "alt": "Symfony",
-        "type": "backend",
-        "link": "https://symfony.com/"
-    },
+  {
+    id: 7,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/color/480/python--v1.png",
+    alt: "python",
+    type: "backend",
+    link: "https://www.python.org/",
+  },
 
-    {
-        "id": 6,
-        "width" : 96,
-        "height": 96,
-        "src": php,
-        "alt": "php",
-        "type": "backend",
-        "link": "https://www.php.net/"
-    },
+  {
+    id: 8,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/fluency/240/nextjs.png",
+    alt: "nextjs",
+    type: "frontend",
+    hover: "https://nextjs.org/",
+  },
 
+  {
+    id: 9,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/fluency/240/bootstrap.png",
+    alt: "bootstrap",
+    type: "css",
+    link: "https://getbootstrap.com/",
+  },
 
-    {
-        "id": 7,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/color/480/python--v1.png",
-        "alt": "python",
-        "type": "backend",
-        "link": "https://www.python.org/"
-    },
+  {
+    id: 10,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/fluency/240/mysql-logo.png",
+    alt: "mysql",
+    type: "database",
+    link: "https://www.mysql.com/fr/",
+  },
 
-    {
-        "id": 8,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/fluency/240/nextjs.png",
-        "alt": "nextjs",
-        "type": "frontend",
-        "hover": "https://nextjs.org/"
-    },
+  {
+    id: 11,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/color/480/javascript--v1.png",
+    alt: "javascript",
+    type: "frontend",
+  },
 
-    {
-        "id": 9,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/fluency/240/bootstrap.png",
-        "alt": "bootstrap",
-        "type": "css",
-        "link": "https://getbootstrap.com/"
-    },
+  {
+    id: 12,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/parakeet/96/sql.png",
+    alt: "sql",
+    type: "database",
+    link: "https://sql.sh/",
+  },
 
-    {
-        "id": 10,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/fluency/240/mysql-logo.png",
-        "alt": "mysql",
-        "type": "database",
-        "link": "https://www.mysql.com/fr/"
-    },
+  {
+    id: 13,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png",
+    alt: "mongodb",
+    type: "database",
+    link: "https://www.mongodb.com/fr-fr",
+  },
 
-    {
-        "id": 11,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/color/480/javascript--v1.png",
-        "alt": "javascript",
-        "type": "frontend",
-    },
+  {
+    id: 14,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/?size=100&id=uJM6fQYqDaZK&format=png&color=000000",
+    alt: "typescript",
+    type: "frontend",
+    link: "https://www.typescriptlang.org/",
+  },
 
-    {
-        "id": 12,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/parakeet/96/sql.png",
-        "alt": "sql",
-        "type": "database",
-        "link": "https://sql.sh/"
-    },
-
-    {
-        "id": 13,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png",
-        "alt": "mongodb",
-        "type": "database",
-        "link": "https://www.mongodb.com/fr-fr"
-    },
-
-    {
-        "id": 14,
-        "width" : 96,
-        "height": 96,
-        "src": "https://img.icons8.com/?size=100&id=uJM6fQYqDaZK&format=png&color=000000",
-        "alt": "typescript",
-        "type": "frontend",
-        "link": "https://www.typescriptlang.org/"
-    },
+  {
+    id: 15,
+    width: 96,
+    height: 96,
+    src: "https://img.icons8.com/sqlite",
+    alt: "sqlite",
+    type: "database",
+    link: "https://www.sqlite.org/",
+  },
 ];
-
 
 export default Language;
