a8b9656a4f1b65d33ce83ef7742398759a7fe203
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-09-04 12:18:25 +0200

    update du header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,7 +5,12 @@ import ThemeSwitches from './ThemeSwitch';
 import { usePathname } from 'next/navigation';
 import { useState, useEffect, useRef } from "react";
 import { X, } from 'lucide-react';
+import { useTheme } from 'next-themes';
 const Header = () => {
+
+  const { theme } = useTheme();
+  const themeClass = (dark: string, light: string) => (theme === "dark" ? dark : light);
+
 
   const [openMenuResponsive, setOpenMenuResponsive] = useState(false);
   const [showMenuAccount, setShowMenuAccount] = useState(false);
@@ -65,12 +70,15 @@ const Header = () => {
             ></div>
 
 
-            <nav className={`${!openMenuResponsive ? "hidden" : "w-[30%] min-lg:hidden  absolute z-50 top-0 bg-gradient-to-tr from-violet-300 to-[#00ABE4]  h-screen max-sm:w-9/12 text-white gap-8 flex flex-col "}`}>
-              <span className="absolute right-0 text-center px-3 pt-1" onClick={() => setOpenMenuResponsive(false)}><X /></span>
+            <nav className={`${!openMenuResponsive ? "hidden" : `w-[30%] min-lg:hidden  absolute z-50 top-0 bg-gradient-to-tr ${themeClass('bg-gray-800', 'bg-gray-50')}  h-screen max-sm:w-9/12 text-white gap-8 flex flex-col`}`}>
+              <span className={`absolute right-0 text-center px-3 pt-1 ${themeClass('text-white', 'text-black')}`} onClick={() => setOpenMenuResponsive(false)}><X /></span>
               <div className="flex items-center flex-col gap-8 text-white mt-20">
-                <Link href="/" className=" text-[#00ABE4] w-35 rounded-[16px] p-3  bg-white hover:bg-white hover:text-black text-center" onClick={() => setOpenMenuResponsive(false)}>Accueil</Link>
-                <Link href="/page/contact" className=" text-[#00ABE4] w-35 rounded-[16px] p-3  bg-white hover:bg-white hover:text-black text-center" onClick={() => setOpenMenuResponsive(false)}>Contact</Link>
-                <Link href="/page/about" className=" text-[#00ABE4] w-35 rounded-[16px] p-3  bg-white hover:bg-white hover:text-black text-center" onClick={() => setOpenMenuResponsive(false)}>A propos</Link>
+                <Link href="/" className=" text-white font-bold w-35 rounded-[16px] p-3  bg-[#00ABE4] hover:bg-[#00ABE4] hover:text-black text-center" onClick={() => setOpenMenuResponsive(false)}>Accueil</Link>
+                <Link href="/page/contact" className=" text-white font-bold w-35 rounded-[16px] p-3  bg-[#00ABE4] hover:bg-[#00ABE4] hover:text-black text-center" onClick={() => setOpenMenuResponsive(false)}>Contact</Link>
+                <Link href="/page/about" className=" text-white font-bold w-35 rounded-[16px] p-3  bg-[#00ABE4] hover:bg-[#00ABE4] hover:text-black text-center" onClick={() => setOpenMenuResponsive(false)}>A propos</Link>
+                <Link href="/page/projects" className=" text-white font-bold w-35 rounded-[16px] p-3  bg-[#00ABE4] hover:bg-[#00ABE4] hover:text-black text-center" onClick={() => setOpenMenuResponsive(false)}>mes projets</Link>
+                <ThemeSwitches />
+
               </div>
             </nav>
           </>
